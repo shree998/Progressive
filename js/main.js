0b3679d47bbf5abe527060cea0123a5c84187374
Shree Nandan Das <shreenandandas@gmail.com>
2020-07-27 23:07:17 +0530

    Modified HTML & DOM

--- js/main.js
+++ js/main.js
@@ -6,3 +6,69 @@ window.onload = () => {
              .register('./sw.js');
   }
 }
+
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.innerHTML = `<span class="strik">${input.value}</span>`;
+	ul.appendChild(li);
+	var but = document.createElement("button");
+	var done = document.createElement("button");
+	done.appendChild(document.createTextNode("Done"));
+	done.classList.add("finish");
+	li.appendChild(done);
+      but.appendChild(document.createTextNode("Delete"));
+	but.classList.add("delete");
+            li.appendChild(but);
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+function strike(event){
+		let par= event.target.parentElement;
+	let child = par.firstChild;
+		console.log('Par first child',child);
+		if(event.target.className==="finish"){
+						par.firstChild.classList.toggle("done");
+						
+						
+		}
+			
+		else{
+			remove(event);
+		}}
+function remove(event){
+	
+	let remove = event.target.parentElement;
+	remove.remove();
+//	let sib = event.target.parentElement;
+//	let rem = sib.previousSibling;
+//	let r = event.target.parentNode;
+//	console.log('PreviousSibling',sib);
+//	console.log('Sib\'s PreviousSibling',rem);
+//	console.log('Previous Element Sibling',event.target.previousElementSibling);
+//	console.log('Parent Element of Delete',r);
+//		ul.removeChild(rem);
+}
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
+ul.addEventListener("click", strike);
